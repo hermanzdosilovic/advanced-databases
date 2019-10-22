b8f12f8ae2fe8a5daf84c298eb81ceb91cc8862c
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2019-10-22 17:18:37 +0200

    Add rank.

--- project-1/migrations/03_create_search_function.sql
+++ project-1/migrations/03_create_search_function.sql
@@ -4,7 +4,8 @@ RETURNS TABLE(
 	title TEXT,
 	categories TEXT,
 	summary TEXT,
-	description TEXT
+	description TEXT,
+	rank FLOAT4
 ) 
 AS 
 $$
@@ -12,8 +13,10 @@ $$
            ts_headline(title, websearch_to_tsquery('english', $1)),
 		   ts_headline(categories, websearch_to_tsquery('english', $1)),
 		   ts_headline(summary, websearch_to_tsquery('english', $1)),
-           ts_headline(description, websearch_to_tsquery('english', $1))
+           ts_headline(description, websearch_to_tsquery('english', $1)),
+		   ts_rank(ARRAY[0.1, 0.2, 0.4, 1.0], alltsv, websearch_to_tsquery('english', $1)) AS rank
     FROM movie
 	WHERE alltsv @@ websearch_to_tsquery('english', $1)
+	ORDER BY rank DESC
 $$ 
 LANGUAGE 'sql';
