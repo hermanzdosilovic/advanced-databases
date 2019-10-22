ad19418d1bfb835c9635d4182d3fffb7c1c988a9
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2019-10-22 16:30:47 +0200

    Refactor migrations and update scripts.

--- project-1/migrations/02_add_tsvectors_to_movie_table.sql
+++ project-1/migrations/02_add_tsvectors_to_movie_table.sql
@@ -2,11 +2,53 @@ ALTER TABLE movie
 ADD COLUMN titletsv TSVector,
 ADD COLUMN categoriestsv TSVector,
 ADD COLUMN summarytsv TSVector,
-ADD COLUMN descriptiontsv TSVector;
+ADD COLUMN descriptiontsv TSVector,
+ADD COLUMN alltsv TSVector;
 
 UPDATE movie 
 SET 
-    titletsv = title::TSVector, 
-    categoriestsv = categories::TSVector,
-    summarytsv = summary::TSVector,
-    descriptiontsv = description::TSVector
+    titletsv = to_tsvector('english', title),
+    categoriestsv = to_tsvector('english', categories),
+    summarytsv = to_tsvector('english', summary),
+    descriptiontsv = to_tsvector('english', description),
+    alltsv = 
+        setweight(to_tsvector('english', title), 'A') ||
+        setweight(to_tsvector('english', categories), 'B') ||
+        setweight(to_tsvector('english', summary), 'C') ||
+        setweight(to_tsvector('english', description), 'D');
+
+CREATE INDEX titletsv_index ON movie USING gin(titletsv);
+CREATE INDEX categoriestsv_index ON movie USING gin(categoriestsv);
+CREATE INDEX summarytsv_index ON movie USING gin(summarytsv);
+CREATE INDEX descriptiontsv_index ON movie USING gin(descriptiontsv);
+CREATE INDEX alltsv_index ON movie USING gin(alltsv);
+
+CREATE TRIGGER movie_titletsv_trigger
+BEFORE INSERT OR UPDATE 
+ON movie
+FOR EACH ROW
+EXECUTE PROCEDURE tsvector_update_trigger(titletsv, 'pg_catalog.english', title);
+
+CREATE TRIGGER movie_categoriestsv_trigger
+BEFORE INSERT OR UPDATE 
+ON movie
+FOR EACH ROW
+EXECUTE PROCEDURE tsvector_update_trigger(categoriestsv, 'pg_catalog.english', categories);
+
+CREATE TRIGGER movie_summarytsv_trigger
+BEFORE INSERT OR UPDATE 
+ON movie
+FOR EACH ROW
+EXECUTE PROCEDURE tsvector_update_trigger(summarytsv, 'pg_catalog.english', summary);
+
+CREATE TRIGGER movie_descriptiontsv_trigger
+BEFORE INSERT OR UPDATE 
+ON movie
+FOR EACH ROW
+EXECUTE PROCEDURE tsvector_update_trigger(descriptiontsv, 'pg_catalog.english', description);
+
+CREATE TRIGGER movie_alltsv_trigger
+BEFORE INSERT OR UPDATE 
+ON movie
+FOR EACH ROW
+EXECUTE PROCEDURE tsvector_update_trigger(alltsv, 'pg_catalog.english', title, categories, summary, description);
--- project-1/migrations/04_create_index_for_movie_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX titletsv_index ON movie USING gin(titletsv);
-CREATE INDEX categoriestsv_index ON movie USING gin(categoriestsv);
-CREATE INDEX summarytsv_index ON movie USING gin(summarytsv);
-CREATE INDEX descriptiontsv_index ON movie USING gin(descriptiontsv);
