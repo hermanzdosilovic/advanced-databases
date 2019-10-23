57762c2f2bfe6fddfa9bf073efc6b79d65310c9e
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2019-10-23 16:44:24 +0200

    Add title_suggestion function.

---
+++ project-1/migrations/04_create_title_suggestion_function.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE FUNCTION title_suggestion(pattern VARCHAR) 
+RETURNS TABLE(
+	title TEXT,
+	categories TEXT
+) 
+AS 
+$$
+SELECT title, summary FROM
+(
+    SELECT word_similarity(title, pattern) AS ts,
+           word_similarity(summary, pattern) AS ss,
+           title,
+           summary
+    FROM movie
+    WHERE word_similarity(title, pattern) > 0.2 OR 
+          word_similarity(summary, pattern) > 0.2
+    ORDER BY ts, ss
+) AS t; 
+$$ 
+LANGUAGE 'sql';
